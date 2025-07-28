d96ee656bfa22c2f5ad3e88398b7a2fc93cac3cd
Pro100den80 <db9656883@gmail.com>
2025-07-28 21:34:01 +0300

    Добавлен комбобокс для вывода тегов

--- Caats.py
+++ Caats.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+Allowed_tags = ["sleep","jump","fight","cute","black","white","play","siamese"]
 
 
 def load_image(url):
@@ -18,7 +21,7 @@ def load_image(url):
 
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
     img = load_image(url_tag)
     if img:
@@ -41,9 +44,6 @@ window.geometry('250x150')
 tag_entry = Entry()
 tag_entry.pack()
 
-load_button = Button(text='Загрузить по тегу', command=open_new_window)
-load_button.pack()
-
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
@@ -54,5 +54,13 @@ file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
 
 url = "https://cataas.com/cat"
+
+tag_label = Label(text='Выберите тэг')
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button(text='Загрузить по тегу', command=open_new_window)
+load_button.pack()
 
 window.mainloop()
